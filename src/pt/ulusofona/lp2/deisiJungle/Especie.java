610830de42d32ba3bc73722116ece29b56dff127
Luiza Vidal <lcopolillo@gmail.com>
2022-11-03 15:25:48 +0000

    atualizar especie

--- src/pt/ulusofona/lp2/deisiJungle/Especie.java
+++ src/pt/ulusofona/lp2/deisiJungle/Especie.java
@@ -4,5 +4,16 @@ public class Especie {
 
     String nome;
     String identificador;
+    String imagem;
+
+    public Especie() {
+
+    }
+
+    public Especie(String nome, String identificador, String imagem) {
+        this.nome = nome;
+        this.identificador = identificador;
+        this.imagem = imagem;
+    }
 
 }
