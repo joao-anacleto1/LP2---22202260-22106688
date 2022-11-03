0e25302783c923c40c34bd5ef2f27a59e9fa4a0f
Joao Anacleto <joaopranacleto@gmail.com>
2022-11-03 17:25:25 +0000

    updates GameManager

--- src/pt/ulusofona/lp2/deisiJungle/GameManager.java
+++ src/pt/ulusofona/lp2/deisiJungle/GameManager.java
@@ -1,5 +1,6 @@
 package pt.ulusofona.lp2.deisiJungle;
 
+import javax.swing.*;
 import java.util.ArrayList;
 
 public class GameManager {
@@ -36,4 +37,49 @@ public class GameManager {
         }
         return specie;
     }
+
+    public boolean createInitialJungle(int jungleSize, int initialEnergy, String[][] playersInfo){
+        return false;
+    }
+
+    public int[] getPlayerIds(int squareNr){
+        return null;
+    }
+
+    public String[] getSquareInfo(int squareNr){
+        return null;
+    }
+
+    public String[] getPlayerInfo(int playerId){
+        return null;
+    }
+
+    public String[] getCurrentPlayerInfo(){
+        return null;
+    }
+
+    public String[][] getPlayersInfo(){
+        return null;
+    }
+
+    public boolean moveCurrentPlayer(int nrSquares, boolean bypassValidations){
+        return false;
+    }
+
+    public String[] getWinnerInfo(){
+        return null;
+    }
+
+    public ArrayList<String> getGameResults(){
+        return new ArrayList<>();
+    }
+
+    public JPanel getAuthorsPanel(){
+        return null;
+    }
+
+    public String whoIsTaborda(){
+        return null;
+    }
+
 }
