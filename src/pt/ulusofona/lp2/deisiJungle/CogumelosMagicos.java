c112b598f840ed2ba28134bd75115a92e4d80da3
Joao Anacleto <joaopranacleto@gmail.com>
2022-12-20 22:14:40 +0000

    atualiza CogumelosMagicos

--- src/pt/ulusofona/lp2/deisiJungle/CogumelosMagicos.java
+++ src/pt/ulusofona/lp2/deisiJungle/CogumelosMagicos.java
@@ -8,7 +8,7 @@ class CogumelosMagicos extends Alimento {
 
     public CogumelosMagicos(){
         this.identificadorAlimento = 'm';
-        this.nomeAlimento = "Cogumelos Magicos";
+        this.nomeAlimento = "Cogumelo Magico";
         this.imagem = "mushroom.png";
         Random r = new Random();
         int min = 10;
