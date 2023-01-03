83f80ee1f13603ee2df4a1bba746373725fec12c
Luiza Vidal <lcopolillo@gmail.com>
2023-01-03 23:08:01 +0000

    create InvalidInitialBoardException

---
+++ src/pt/ulusofona/lp2/deisiJungle/InvalidInitialBoardException.java
@@ -0,0 +1,18 @@
+package pt.ulusofona.lp2.deisiJungle;
+
+public class InvalidInitialBoardException {
+
+
+    public String getMessage() {
+        return "";
+    }
+
+    public boolean isInvalidPlayer(){
+        return false;
+    }
+
+    public boolean isInvalidFood(){
+        return false;
+    }
+    
+}
