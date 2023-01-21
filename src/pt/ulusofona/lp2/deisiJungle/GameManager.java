923e071a821432da5e9b22484d8a675faeeae589
Joao Anacleto <joaopranacleto@gmail.com>
2023-01-21 00:45:56 +0000

    atualiza getWinnerInfo v4

--- src/pt/ulusofona/lp2/deisiJungle/GameManager.java
+++ src/pt/ulusofona/lp2/deisiJungle/GameManager.java
@@ -514,53 +514,6 @@ public class GameManager {
                     posicaoChegada += 1;
                 }
             }
-        } else if (jogoAcabado() == 3) {
-
-            int metade;
-
-            if(mapa.buscarTamanhoMapa()/2 % 2 == 0){
-                metade = mapa.buscarTamanhoMapa()/2;
-            }else {
-                metade = mapa.buscarTamanhoMapa()/2 + 1;
-            }
-
-            Jogador jogador = mapa.buscarCasa(metade).buscarJogadores().get(0);
-
-            for(int i = 0; i < mapa.buscarCasa(metade).buscarJogadores().size(); i++){
-
-                if(mapa.buscarCasa(metade).buscarJogadores().get(i).buscarEnergia() > jogador.buscarEnergia() ){
-
-                    jogador = mapa.buscarCasa(metade).buscarJogadores().get(i);
-                }
-
-            }
-
-            String res = "#" + posicaoChegada + " " + jogador.buscarNome() + ", " +
-                    jogador.buscarNomeEspecie() + ", " + jogador.buscarPosicaoAtual() +
-                    ", " + jogador.buscarPosicaoPercorrida() + ", " + jogador.buscarSomarComida();
-            resultado.add(res);
-            posicaoChegada += 1;
-
-            for (int i = mapa.buscarTamanhoMapa(); i > 0; i--) {
-                ArrayList<Jogador> jogadores = mapa.buscarCasa(i).ordernarIds();
-
-                for (Jogador jogadore : jogadores) {
-                    
-                    if(jogadore.buscarId() != jogador.buscarId()) {
-                        String nomeJogador = jogadore.buscarNomeJogador();
-                        String nomeEspecie = jogadore.buscarNomeEspecie();
-                        int posicaoNoMapa = jogadore.buscarPosicaoAtual();
-                        res = "#" + posicaoChegada + " " + nomeJogador + ", " + nomeEspecie + ", " + posicaoNoMapa +
-                                ", " + jogadore.buscarPosicaoPercorrida() + ", " + jogadore.buscarSomarComida();
-                        resultado.add(res);
-                        posicaoChegada += 1;
-                    }
-                }
-            }
-
-
-
-
         }
 
         else {
